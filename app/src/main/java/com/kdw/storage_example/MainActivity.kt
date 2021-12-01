82f9d8cf73ce22e4255b7784f8d592aa23f04dee
Dongkw324 <59963677+Dongkw324@users.noreply.github.com>
2021-12-01 09:58:08 +0900

    Storage 예제

--- app/src/main/java/com/kdw/storage_example/MainActivity.kt
+++ app/src/main/java/com/kdw/storage_example/MainActivity.kt
@@ -1,13 +1,36 @@
 package com.kdw.storage_example
 
+import android.database.ContentObserver
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.IntentSenderRequest
+import com.kdw.storage_example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var internalPhotoAdapter: InternalPhotoAdapter
+    private lateinit var externalPhotoAdapter: ExternalPhotoAdapter
+
+    private var readPermission = false
+    private var writePermission = false
+
+    private lateinit var permissionLauncher : ActivityResultLauncher<Array<String>>
+    private lateinit var intentSenderLauncher: ActivityResultLauncher<IntentSenderRequest>
+
+    private lateinit var contentObserver: ContentObserver
+
+    private var deletedImageUri : Uri? = null
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
 
     }
 }
